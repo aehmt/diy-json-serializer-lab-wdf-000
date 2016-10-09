10d70436a53de02897d4c99fa38909486eacc699
aehmt <calisahmet@gmail.com>
2016-10-09 19:19:31 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def next
+    Product.where("id > ?", id).limit(1).first
+  end
+
+  def prev
+    Product.where("id < ?", id).limit(1).first
+  end
 end
